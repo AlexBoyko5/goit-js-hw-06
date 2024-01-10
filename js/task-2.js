8bbd943cb2698e87b42d8d625429d0b0dd5b2aeb
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-01-10 16:43:10 +0100

    update-Task2
    still have question with outputting an extra element to the console

--- js/task-2.js
+++ js/task-2.js
@@ -15,6 +15,7 @@ class Storage {
 }
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
 storage.addItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 storage.removeItem("Prolonger");
